8d593f5621ec8f67387d9782dda5fcde33b2add3
Lucas Salvatori <lucas.o.salvatori@gmail.com>
2025-05-08 18:45:46 -0300

    Invertimos cajas

--- js/cajas.js
+++ js/cajas.js
@@ -7,7 +7,7 @@ console.log(containBox);
 // containBox.innerHTML += `<div class="box">3</div>`;
 
 // Opcion iteracion
-function generateBoxes(count = 20) {
+function generateBoxes(count = 5) {
   for (let index = 0; index < count; index++) {
     containBox.innerHTML += `<div class="box">${index + 1}</div>`;
   }
@@ -20,4 +20,25 @@ function invertir() {
   // Que pasaria si utilizaramos getElementsByClassName()
   const boxList = document.querySelectorAll(".box");
   console.log(boxList);
+}
+
+function pruebaListas() {
+  const listaEstatica = document.querySelectorAll(".box");
+  const listaDinamica = document.getElementsByClassName("box");
+  console.log("lista Estatica", listaEstatica);
+  console.log("lista dinamica", listaDinamica);
+}
+
+function cambiarCaja() {
+  document.getElementById("cambio").innerText = "aasdsgjshvk seo";
+}
+
+function invertirConCSS() {
+  document.getElementById("container").classList.toggle("invertir");
+}
+
+function invertirConArray() {
+  const lista = document.querySelectorAll(".box");
+  const array = Array.from(lista);
+  console.log(array.reverse());
 }
